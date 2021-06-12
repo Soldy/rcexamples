d903def88ea1fa78c1ad71b4d585568108fcb65f
Soldy <4786022+Soldy@users.noreply.github.com>
2021-06-12 22:58:19 +0100

    fiveormore
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ fiveormore.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+
+const fiveormoreBase =  function(){
+    const _table = []
+    const _makeTable = function(){
+        _table =[];
+        for(let x = 0 ; 10 > x ;x++){
+            _table.push([]);
+            for(let y = 0 ; 10 > y ; y++)
+                _table[x].push(0);
+        }
+    }
+
+}
