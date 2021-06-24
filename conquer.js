b202b225582538921658fcb8b69dad06a6390716
Soldy <4786022+Soldy@users.noreply.github.com>
2021-06-24 22:46:49 +0100

    conqueror list natural planets
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- conquer.js
+++ conquer.js
@@ -3,28 +3,34 @@
 
 
 const conquerBase =  function(){
-    let _planets = [];
-    let players = 2;
+    let _planets = {};
+    let _players_count = 2;
+    let _players_ = {};
     const _universeGenerator = function(){
         _planets = [];
         let serial = 0;
         for (y = 0 ; 16 > y; y++)
             for (x = 0 ; 16 > x; x++){
-                if(5 > Math.round(Math.random()*20))
-                    planet.push({
+                if(5 > Math.round(Math.random()*20)){
+                    _planets['planet_'+serial]={
                         'name'      : serial.toString(32),
                         'x'         : x,
                         'y'         : y,
                         'flot'      : [],
+                        'natural'   : true,
                         'buildings' : [],
                         'moons'     : []
                     });
-                    serial+;
-                ;
+                    serial+;
+                }
             }
-                 
-            
-
+    }
+    const _naturalPlanets = function(){
+        let out =[];
+        for (let i of _planets)
+            if(i.natural)
+                out.push(i.name);
+        return out;
     }
 
 }
